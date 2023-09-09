6387ed776c6fba225708999176832fcad01dd805
VotreNom <shawanbarua70@gmail.com>
2023-09-09 18:24:43 +0200

    factorial

---
+++ simple_task/factoriale.java
@@ -0,0 +1,12 @@
+public class factoriale {
+
+    public static void main(String[] args) {
+        int n = 5;
+        int result = 1;
+
+        for (int i = 1;i <= n;i++){
+            result = result * i;
+        }
+        System.out.println(result);
+    }
+}
