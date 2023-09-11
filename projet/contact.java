1caa996423960c6cf8406ed3baab3f86d4094e6f
VotreNom <shawanbarua70@gmail.com>
2023-09-11 21:02:11 +0200

    first project

---
+++ projet/contact.java
@@ -0,0 +1,26 @@
+package projet;
+
+public class contact {
+    String firstName;
+    String lastName;
+    String PhoneNumber;
+
+    public String getFirstName() {
+        return firstName;
+    }
+     public void setFirstName(String firstName){
+        this.firstName= firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.PhoneNumber = phoneNumber;
+    }
+}
--- projet/main.java
+++ projet/main.java
@@ -9,9 +9,17 @@ public class main {
     src = new Scanner(System.in);
     String firstName = getUser("You  firstname");
     String lastName = getUser("You lastname");
+    String PhoneNumber = getUser("your phone number");
     src.close();
+    contact newContact = new contact();
+
+    newContact.setFirstName(firstName);
+    newContact.setLastName(lastName);
+    newContact.setPhoneNumber(PhoneNumber);
+
     System.out.println(lastName);
     System.out.println(firstName);
+    System.out.println(PhoneNumber);
     }
 
     public static String getUser(String request){
